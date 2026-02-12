3ffb7ad0b16b6adb18c73440101acc226ab106d1
sirenwah <siren_w@web.de>
2026-02-12 14:34:52 +0100

    Update app.py
    datenbankfix

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fahrtenbuch_postgres_sql_u
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
 
 # --- Models ---
 class User(db.Model):
