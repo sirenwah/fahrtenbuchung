aa08a7700a75ce4d592905377195fcca6b24744f
sirenwah <siren_w@web.de>
2026-02-12 13:43:32 +0100

    Update app.py
    Datenbankfix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dein_geheimer_schluessel_2024'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fahrten.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/fahrten.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
